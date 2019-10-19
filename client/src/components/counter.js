28843bc6320f394af49490740432bb663762e521
Xybolx <xybolx@gmail.com>
2019-10-19 10:42:52 -0700

    lesson

--- client/src/components/counter.js
+++ client/src/components/counter.js
@@ -1,50 +1,32 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import useCounter from "./useCounter";
 
-class Counter extends Component {
+const Counter = () => {
 
-        // state
-        state = {
-            count: 0
-        };
+    // state
+    const [count, increment, decrement, clear] = useCounter(0);
 
-        // function to increment count
-        increment = () => {
-            this.setState({ count: this.state.count + 1 });
-        };
-
-        // function to decrement count
-        decrement = () => {
-            this.setState({ count: this.state.count - 1 });
-        };
-
-        // function to clear count to 0
-        clear = () => {
-            this.setState({ count: 0 });
-        };
-
-    render() {
-        return (
-            <div>
-                <h1 className="display-4">Hooks Counter</h1>
-                <h2 className="lead">Count is: <span className="font-weight-bold text-danger">{this.state.count}</span></h2>
-                <button
-                    className="btn btn-danger"
-                    onClick={this.decrement}>
-                    - 1
+    return (
+        <div>
+            <h1 className="display-4">Hooks Counter</h1>
+            <h2 className="lead">Count is: <span className="font-weight-bold text-danger">{count}</span></h2>
+            <button
+                className="btn btn-danger"
+                onClick={decrement}>
+                - 1
                 </button>
-                <button
-                    className="btn btn-success mx-1"
-                    onClick={this.increment}>
-                    + 1
+            <button
+                className="btn btn-success mx-1"
+                onClick={increment}>
+                + 1
                 </button>
-                <button
-                    className="btn btn-outline-warning"
-                    onClick={this.clear}>
-                    CLR
+            <button
+                className="btn btn-outline-warning"
+                onClick={clear}>
+                CLR
                 </button>
-            </div>
-        );
-    };
+        </div>
+    );
 };
 
 export default Counter;
--- client/src/components/useCounter.js
+++ client/src/components/useCounter.js
@@ -2,19 +2,19 @@ import { useState } from "react";
 
 const useCounter = initial => {
     // state
-    // const [value, setValue] = useState(initial);
+    const [value, setValue] = useState(initial);
 
     // function to increment count by 1
-    // const increment = () => setValue(value => value + 1);
+    const increment = () => setValue(value => value + 1);
 
     // function to decrement count by 1
-    // const decrement = () => setValue(value => value - 1);
+    const decrement = () => setValue(value => value - 1);
 
     // function to clear count
-    // const clear = () => setValue(initial);
+    const clear = () => setValue(initial);
 
     // return an array
-    // return [value, increment, decrement, clear];
+    return [value, increment, decrement, clear];
 
 };
 
